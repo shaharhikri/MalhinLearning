b60eab568ed0ea9df199598c0e90cf1f36ca1270
Shahar <shaharhikri@ravendb.net>
2022-03-08 16:12:29 +0200

    JWT: started working on jwt auth

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -4,52 +4,54 @@ dotenv.config();
 const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
 const path = require('path');
 
 const uploadPath = require(path.join(__dirname, '../services/uploadsPathService'));
 const genUUID = require(path.join(__dirname, '../services/uuidFactory'));
 const ravendb = require(path.join(__dirname, '../dbUtils/common'));
 const { User } = require('../dbUtils/modelClasses');
-
-const initializePassport = require(path.join(__dirname, '../services/passport-config'));
-initializePassport(
-    passport,
-    ravendb.findUserByEmail,
-    ravendb.findUserById
-)
+const jwt = require('jsonwebtoken')
 
 router.use(express.urlencoded({ extended: false }))
-router.use(flash())
-router.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false, 
-    cookie: {
-        secure: false,
-        maxAge: 2592000000
-    }
-}))
-router.use(passport.initialize())
-router.use(passport.session())
 
-router.get('/login', checkNotAuthenticated, (req, res) => {
+router.get('/login', authenticateToken, (req, res) => {
     res.render(path.join(__dirname, '../static/login.ejs'))
 })
 
-router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+router.post('/login', async (req, res) => {
+    const user = await ravendb.findUserByEmail(req.body.email);
+    try{
+        console.log(req.body.password,  user.salt, user.hashedpassword)
+        if ( await bcrypt.compare(req.body.password, user.salt+user.hashedpassword)){
 
-router.get('/register', checkNotAuthenticated, (req, res) => {
+            //return token
+            const userId = { userid : user.id }
+
+            const accessToken = jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET) //, { expiresIn: 300})
+            res.json({ accessToken : accessToken })
+
+            //redirect
+            res.redirect('/');
+            console.log('logged in')
+        }
+        else{
+            res.redirect('back');
+            console.log('log in failed')
+        }
+    }
+    catch(e){
+        console.log(e)
+        res.redirect('back');
+    }
+
+})
+
+router.get('/register', (req, res) => {
     res.render(path.join(__dirname, '../static/register.ejs'))
 })
 
-router.post('/register', checkNotAuthenticated, async (req, res) => {
+router.post('/register', async (req, res) => {
+    console.log(req.body)
     try {
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -65,29 +67,56 @@ router.post('/register', checkNotAuthenticated, async (req, res) => {
 
         res.redirect('/login');
     } catch (e){
+        console.log(e)
         res.redirect('/register');
     }
 });
 
 
 router.post('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
+
 })
 
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-
+    // if (isAuthenticated()) {
+        // return next()
+    // }
     res.redirect('/login')
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {      
+    // if (isAuthenticated()) {      
         return res.redirect('/')
-    }
-    next()
+    // }
+    // next()
 }
+
+function isAuthenticated(userid, userToken) {
+    return false;
+}
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if ( token === null){
+        //return res.sendStatus(401)
+        next()
+    }
+    else{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decriptedToken) => {
+            if (err){
+                //return res.sendStatus(403)
+                next()
+            }
+            else{
+                req.decriptedToken = decriptedToken
+                console.log(decriptedToken)
+                res.redirect('/');
+            }
+        })
+    }
+    
+}
+
 
 module.exports = [ router, checkAuthenticated ];
