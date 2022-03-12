18bfc091a1e4943f36e5c106f2e610a7817a7a10
Shahar Hikri <shaharhikri@gmail.com>
2022-03-12 18:31:10 +0200

    main: started working on uploading output files as ravendb attachments.

--- src/dbUtils/common.js
+++ src/dbUtils/common.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const fs = require('fs-extra');
 const path = require('path');
 const documentStore = require(path.join(__dirname, './documentStore'));
 const { User } = require('./modelClasses');
 
 function storeUser(userEntity){
     try{
-        session = documentStore.openSession();
+        let session = documentStore.openSession();
         session.store(userEntity, 'Users/'+userEntity.id);
         session.saveChanges();
     }
@@ -16,8 +19,8 @@ function storeUser(userEntity){
 
 async function findUserById(id){
     try{
-        session = documentStore.openSession();
-        var user = await session.session.load(id); 
+        let session = documentStore.openSession();
+        let user = await session.session.load(id); 
         return user;
     }
     catch (e){
@@ -28,12 +31,52 @@ async function findUserById(id){
 
 async function findUserByEmail(email){
     try{
-        session = documentStore.openSession();
-        var users = await session.query(User).whereEquals('email', email).all(); 
+        let session = documentStore.openSession();
+        let users = await session.query(User).whereEquals('email', email).all(); 
         return users[0];
     }
     catch (e){
         console.log('common::findUserByEmail - RavenException');
+        return null;
+    }
+}
+
+async function storeAttachment(id, fileName, attachmentName){
+    try{
+        let session = documentStore.openSession();
+        const file = fs.createReadStream(fileName);
+        session.advanced.attachments.store(id, attachmentName, file, "text/plain");
+        await session.saveChanges();
+        return true;
+    }
+    catch (e){
+        console.log('common::storeAttachment - RavenException');
+        return false;
+    }
+}
+
+async function getAttachmentsInfo(id){
+    try{
+        let session = documentStore.openSession();
+        let user = await session.session.load(id)
+        const names = await session.advanced.attachments.getNames(user);
+        return names;
+    }
+    catch (e){
+        console.log('common::getAttachmentsNames - RavenException', e);
+        return [];
+    }
+}
+
+async function getAttachment(id, filename){
+    try{
+        let session = documentStore.openSession();
+        // let user = await session.session.load(id);
+        const attachmentStream = await session.advanced.attachments.get(id, filename);
+        return attachmentStream.data;
+    }
+    catch (e){
+        console.log('common::attachmentStream - RavenException', e);
         return null;
     }
 }
@@ -45,4 +88,7 @@ else{
     module.exports.storeUser = storeUser;
     module.exports.findUserById = findUserById;
     module.exports.findUserByEmail = findUserByEmail;
+    module.exports.storeAttachment = storeAttachment;
+    module.exports.getAttachmentsInfo = getAttachmentsInfo;
+    module.exports.getAttachment = getAttachment;
 }
