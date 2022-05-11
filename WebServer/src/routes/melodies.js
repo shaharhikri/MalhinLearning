5b90b5b8ce8b962afcd5f0aaf33b28894632755a
Shahar Hikri <shaharhikri@gmail.com>
2022-05-11 19:02:30 +0300

    main: Fix melodies authorization bug

--- WebServer/src/routes/melodies.js
+++ WebServer/src/routes/melodies.js
@@ -4,7 +4,7 @@ dotenv.config();
 const express = require('express'); // Express Web Server
 const path = require('path');
 const ravendb = require(path.join(__dirname, '../dbUtils/common'));
-const { tokenConnectedAuthenticationMiddleware } = require(path.join(__dirname, '../services/userSignAuthentication'));
+const { tokenActionAuthorizationMiddleware } = require(path.join(__dirname, '../services/userActionAuthorization'));
 const bodyParser = require('body-parser')
 
 const router = express.Router();
@@ -12,7 +12,7 @@ router.use(bodyParser.json());
 
 
 //For example: http://localhost:8080/melodies/download/input_file_waltzes_02-04-2022_19-19-27-495.midi
-router.get("/download/:attName", tokenConnectedAuthenticationMiddleware, async (req, res) => {
+router.get("/download/:attName", tokenActionAuthorizationMiddleware, async (req, res) => {
     let stream = await ravendb.getAttachment(req.user.id, req.params.attName);
     if ( stream ){
         stream.pipe(res);
@@ -23,7 +23,7 @@ router.get("/download/:attName", tokenConnectedAuthenticationMiddleware, async (
 });
 
 //For example: DELETE http://localhost:8080/melodies/delete/input_file_waltzes_02-04-2022_19-19-27-495.midi
-router.delete("/delete/:attName", tokenConnectedAuthenticationMiddleware, async (req, res) => {
+router.delete("/delete/:attName", tokenActionAuthorizationMiddleware, async (req, res) => {
     if(await ravendb.deleteAttachment(req.user.id, req.params.attName)){
         res.status(200).send();
     }
@@ -33,7 +33,7 @@ router.delete("/delete/:attName", tokenConnectedAuthenticationMiddleware, async 
 });
 
 
-router.get("/getattachmentsnames", tokenConnectedAuthenticationMiddleware, async (req, res) => {
+router.get("/getattachmentsnames", tokenActionAuthorizationMiddleware, async (req, res) => {
     try{
         let infoList = await ravendb.getAttachmentsInfo(req.user.id);
         if ( !infoList || !Array.isArray(infoList)) {
