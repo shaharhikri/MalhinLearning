751586f36e72135d94713a3b039f6df9ba22c6ee
Shahar Hikri <shaharhikri@gmail.com>
2022-05-18 17:33:29 +0300

    main : FIX LOGGIN BUG

--- WebServer/src/dbUtils/modelClasses.js
+++ WebServer/src/dbUtils/modelClasses.js
@@ -2,7 +2,7 @@ class User {
     constructor(id, name, email, salt, hashedpassword) {
         this.id = id;
         this.name = name;
-        this.email = email.toLowerCase();
+        this.email = email?.toLowerCase();
         this.salt = salt;
         this.hashedpassword = hashedpassword;
     }
